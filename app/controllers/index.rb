a174318e1bc8045e5b79c5cbb8c98dc5e4386ab0
hypatiah <danielle.hassid@gmail.com>
2016-07-25 17:45:32 -0700

    Add errors for registering

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,8 @@
 get '/' do
     erb :index
 end
+
+get '/dashboard' do
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    erb :dashboard
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 get '/users/new' do
     erb :'users/new'
 end
+
+post '/users/new' do
+    if params[:password1] == params[:password2]
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password2])
+    else @error = "Sorry, passwords don't match"
+         erb :'/users/new'
+    end
+    if @user.save
+        login(@user)
+        redirect '/dashboard'
+    elsif @user.username == User.find_by(username: @user.username)
+        @error = 'Sorry, username already exits'
+        erb :'/users/new'
+    elsif @user.email == User.find_by(email: @user.email)
+        @error = 'Sorry, email already exits'
+        erb :'/users/new'
+    end
+end
