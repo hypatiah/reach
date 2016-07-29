1b27d57b015c1a60809a54912d3e97453140f283
hypatiah <danielle.hassid@gmail.com>
2016-07-29 15:35:42 -0700

    Center pages and finish buttons

--- app/controllers/reaches_controller.rb
+++ app/controllers/reaches_controller.rb
@@ -29,10 +29,12 @@ end
 
 put '/reaches/:id' do
   @reach = Reach.find(params[:id])
-  p "*" * 20
-  p params
+  # p "*" * 20
+  # p params
   @reach.update(params[:reach])
   @user = User.find(session[:user_id])
+  # p "* " * 20
+  # p @user.reaches.where(main_reach: true)
   if params[:reach]['main_reach'] == "on"
     @user.reaches.where(main_reach: true).each do |reach|
       reach.main_reach = false
